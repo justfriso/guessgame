b676ab57987dc6b5403d92f5fa8780b2ddfc60e3
justfriso <huizengafriso@gmail.com>
2023-05-16 13:01:40 +0200

    added a class to be able to loop the game.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);//creating scanner object.
+        again dinges = new again();
+        Scanner input = new Scanner(System.in);
 
         int RandomNumber = (int) (Math.random() * 1001);//generating a number between 0 and 1000.
 
@@ -13,14 +14,19 @@ class Main {
 
         System.out.println("You have 10 attempts to guess a number between 0-1000");
 
-        for (i = 0; i < attempts; i++) {//making sure we haven't exceeded 10 attempts and adding +1 to the attempts.
-            System.out.println("Enter your guess:");
+        for (i = 0; i < attempts; i++) {
+            System.out.println("Enter your guess: ");
 
             guess = input.nextInt();
 
             if (RandomNumber == guess) {
                 System.out.println("Correct!");
-                break;
+                System.out.println();
+                System.out.println("Would you like to play again?(1 for yes): ");//It's not perfect yet because I still have to implement a different way to say yes and no.
+                int answer = input.nextInt();
+                if(answer == 1){
+                    again.playagain();}
+                else {break;}
             } else if (RandomNumber > guess && i != attempts - 1) {
                 System.out.println("Too low, try again.");
             } else if (RandomNumber < guess && i != attempts - 1) {
@@ -30,6 +36,14 @@ class Main {
         if (i == attempts) {
             System.out.println("You have used all your attempts");
             System.out.printf("The number was: "+ RandomNumber);
+            System.out.println();
+            System.out.println("Would you like to play again?(1 for yes): ");
+            int answer = input.nextInt();
+            if(answer == 1){
+                again.playagain();
+            }else {
+                return;}
         }
+
     }
 }
---
+++ src/again.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class again {
+
+    public static void playagain() {
+
+        Main.main(null);
+    }
+}
