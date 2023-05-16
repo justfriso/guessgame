8a8cd4ed1c222ca82e45d67226c300ef954b3590
justfriso <huizengafriso@gmail.com>
2023-05-16 11:17:53 +0200

    added ability to guess 10 times.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,35 @@
 import java.util.Scanner;
 class Main {
-    public static void main(String[] args){
-        int RandomNumber = (int) (Math.random() * 1001);
-        System.out.println(RandomNumber);//for a random number between 0 and 1000.
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter guess: ");
-        int Guess1 = input.nextInt();
-        if (Guess1 > RandomNumber){
-            System.out.println("Too high, try again.");}
-        else if (Guess1 < RandomNumber){
-            System.out.println("Too low, try again.");}
-        else {
-            System.out.println("Correct!");}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);//creating scanner object.
+
+        int RandomNumber = (int) (Math.random() * 10);//generating a number between 0 and 1000.
+
+        System.out.println(RandomNumber);//printing the number.
+
+        int attempts = 10;//the amount of attempts to guess the number.
+
+        int i, guess;
+
+        System.out.println("You have 10 attempts to guess a number between 0-1000");
+
+        for (i = 0; i < attempts; i++) {//making sure we haven't exceeded 10 attempts and adding +1 to the attempts.
+            System.out.println("Enter your guess:");
+
+            guess = input.nextInt();
+
+            if (RandomNumber == guess) {
+                System.out.println("Correct!");
+                break;
+            } else if (RandomNumber > guess && i != attempts - 1) {
+                System.out.println("Too low, try again.");
+            } else if (RandomNumber < guess && i != attempts - 1) {
+                System.out.println("Too high, try again.");
+            }
+        }
+        if (i == attempts) {
+            System.out.println("You have used all your attempts");
+            System.out.printf("The number was: "+ RandomNumber);
+        }
     }
 }
