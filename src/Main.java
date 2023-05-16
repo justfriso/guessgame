7aa0a7a856bb79682f6c1a342b99905079513909
justfriso <huizengafriso@gmail.com>
2023-05-16 09:57:10 +0200

    small test change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println((int)(Math.random() * 1001));//for a random number between 0 and 1000
+        System.out.println((int)(Math.random() * 1001));//for a random number between 0 and 1000.
     }
 }
