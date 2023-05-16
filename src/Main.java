a5dd8e1c3b9765d57f745b6cce2e5cbcf173ab5b
justfriso <huizengafriso@gmail.com>
2023-05-16 10:41:10 +0200

    feedback to user given

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
+import java.util.Scanner;
+class Main {
     public static void main(String[] args){
-        System.out.println((int)(Math.random() * 1001));//for a random number between 0 and 1000.
+        int RandomNumber = (int) (Math.random() * 1001);
+        System.out.println(RandomNumber);//for a random number between 0 and 1000.
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter guess: ");
+        int Guess1 = input.nextInt();
+        if (Guess1 > RandomNumber){
+            System.out.println("Too high, try again.");}
+        else if (Guess1 < RandomNumber){
+            System.out.println("Too low, try again.");}
+        else {
+            System.out.println("Correct!");}
     }
 }
